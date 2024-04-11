e7e31fc6d651e2a4574f6e8183ab55e483bfafb6
Valdir Júnior <valdirmsjunior@gmail.com>
2024-04-11 17:15:24 -0300

    finalizando desafio do projeto poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "45678", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
